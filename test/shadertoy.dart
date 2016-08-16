bf38464b00041f572feba45d08028f67faa45b2b
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-08-16 15:44:44 +0200

    Fix formatting of mandelbrot shader

--- test/shadertoy.dart
+++ test/shadertoy.dart
@@ -18,42 +18,44 @@ varying vec2 position;
 uniform float viewportWidth, viewportHeight;
 
 vec3 hsv2rgb(vec3 c) {
-vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
 
 void main() {
-int iter;
-float tempreal, tempimag, Creal, Cimag;
-float r2;
+  int iter;
+  float tempreal, tempimag, Creal, Cimag, r2;
 
-vec2 viewport = vec2(viewportHeight, viewportHeight);
-vec2 pos = fract((position + viewport) /
-(2.0 * vec2(viewportHeight, viewportHeight)));
-float real = (pos.s * 3.0) - 2.0;
-float imag = (pos.t * 3.0) - 1.5;
-Creal = real;
-Cimag = imag;
+  vec2 viewport = vec2(viewportHeight, viewportHeight);
+  vec2 pos = fract((position + viewport) /
+    (2.0 * vec2(viewportHeight, viewportHeight)));
 
-for (iter = 0; iter < 100; iter++) {
-// z = z^2 + c
-tempreal = real;
-tempimag = imag;
-real = (tempreal * tempreal) - (tempimag * tempimag);
-imag = 2 * tempreal * tempimag;
-real += Creal;
-imag += Cimag;
-r2 = (real * real) + (imag * imag);
-if (r2 >= 4)
-  break;
-}
+  float real = (pos.s * 3.0) - 2.0;
+  float imag = (pos.t * 3.0) - 1.5;
+  Creal = real;
+  Cimag = imag;
 
-// Base the color on the number of iterations
-float value = fract(iter / 100.0);
-vec4 color = vec4(hsv2rgb(vec3(value, 0.8, 0.8)), 1.0);
+  for (iter = 0; iter < 100; iter++) {
+    // z = z^2 + c
+    tempreal = real;
+    tempimag = imag;
+    real = (tempreal * tempreal) - (tempimag * tempimag);
+    imag = 2 * tempreal * tempimag;
+    real += Creal;
+    imag += Cimag;
+    r2 = (real * real) + (imag * imag);
 
-gl_FragColor = color;
+    if (r2 >= 4) {
+      break;
+    }
+  }
+
+  // Base the color on the number of iterations
+  float value = fract(iter / 100.0);
+  vec4 color = vec4(hsv2rgb(vec3(value, 0.8, 0.8)), 1.0);
+
+  gl_FragColor = color;
 }
 ''';
 
