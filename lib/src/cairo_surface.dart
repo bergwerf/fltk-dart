8132a02ca5281290614747ca3f61c7e8a086d964
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-08-25 19:34:50 +0200

    Add CairoSurface

---
+++ lib/src/cairo_surface.dart
@@ -0,0 +1,56 @@
+// Copyright (c) 2016, Herman Bergwerf. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+part of fltk;
+
+/// Special implementation of Cairo using an [ImageSurface] from `cairodart` and
+/// the `fl_draw_image` function from FLTK.
+class CairoSurface extends Widget {
+  /// Target [ImageSurface] (drawing canvas)
+  cairo.ImageSurface _surface;
+
+  /// Redraw event stream
+  Stream<cairo.Context> onDraw;
+
+  /// Redraw stream controller
+  /// Note that this is a synchronous stream because all redraws have to be
+  /// completed before drawing the surface data to the widget.
+  final _onDrawController =
+      new StreamController<cairo.Context>.broadcast(sync: true);
+
+  CairoSurface(int x, int y, int w, int h) : super(x, y, w, h) {
+    onDraw = _onDrawController.stream;
+
+    // Initialize drawing surface.
+    resizeSurface(w, h);
+  }
+
+  /// Resize [_surface].
+  void resizeSurface(int width, int height) {
+    if (_surface != null) {
+      _surface.finish();
+    }
+    _surface = new cairo.ImageSurface.forData(
+        new List<int>.filled(width * height * 4, 128),
+        cairo.Format.ARGB32,
+        width,
+        height,
+        cairo.Format.ARGB32.strideForWidth(width));
+  }
+
+  /// Redraw surface
+  ///
+  /// Do not override this method! To draw, you should draw directly to the
+  /// image surface.
+  void draw() {
+    // Trigger surface updates.
+    final ctx = new cairo.Context(_surface);
+    _onDrawController.add(ctx); // Synchronous processing
+    _surface.flush();
+
+    // Draw canvas to widget.
+    final bytes = new Uint8List.fromList(_surface.data);
+    drawImage(bytes, x, y, _surface.width, _surface.height, 4);
+  }
+}
---
+++ test/cairo_surface.dart
@@ -0,0 +1,64 @@
+// Copyright (c) 2016, Herman Bergwerf. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+import 'dart:math';
+
+import 'package:cairodart/cairodart.dart';
+import 'package:fltk/fltk.dart' as fl;
+
+int main() {
+  final window = new fl.DoubleWindow(300, 300, 'Dart');
+  final surface = new fl.CairoSurface(0, 0, window.w, window.h);
+
+  window.resizable = surface;
+  window.color = fl.RED;
+
+  // Set cairo draw callback.
+  surface.onDraw.listen((Context ctx) {
+    ctx.lineWidth = 1.0;
+
+    // Final variables
+    final r1 = 80, r2 = 40, cx = surface.w / 2, cy = surface.h / 2;
+
+    // Red circle
+    var a = PI / 2;
+    ctx
+      ..save()
+      ..translate(cx + r2 * cos(a), cy - r2 * sin(a))
+      ..setSourceRgb(0, 0, 0)
+      ..arc(0, 0, r1, 0, 2 * PI)
+      ..strokePreserve()
+      ..setSourceRgba(1, 0, 0, 0.7)
+      ..fill()
+      ..restore();
+
+    // Green circle
+    a = PI / 6 * 7;
+    ctx
+      ..save()
+      ..translate(cx + r2 * cos(a), cy - r2 * sin(a))
+      ..setSourceRgb(0, 0, 0)
+      ..arc(0, 0, r1, 0, 2 * PI)
+      ..strokePreserve()
+      ..setSourceRgba(0, 1, 0, 0.7)
+      ..fill()
+      ..restore();
+
+    // Blue circle
+    a = PI / 6 * -1;
+    ctx
+      ..save()
+      ..translate(cx + r2 * cos(a), cy - r2 * sin(a))
+      ..setSourceRgb(0, 0, 0)
+      ..arc(0, 0, r1, 0, 2 * PI)
+      ..strokePreserve()
+      ..setSourceRgba(0, 0, 1, 0.7)
+      ..fill()
+      ..restore();
+  });
+
+  window.show();
+
+  return fl.run();
+}
